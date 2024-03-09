68b5143d7c6baa95bee846fd3ce908614824c000
noahrjh <jacksnu@mail.uc.edu>
2024-03-09 18:43:31 -0500

    Add project files.

---
+++ Todo/Data/TodoItemDatabase.cs
@@ -0,0 +1,58 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Models;
+
+namespace Todo.Data
+{
+    internal class TodoItemDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public TodoItemDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<TodoItem>();
+        }
+
+        public Task<List<TodoItem>> GetItemAsync()
+        {
+            return _database.Table<TodoItem>().ToListAsync();
+        }
+
+        public Task<List<TodoItem>> GetItemsNotDoneAsync()
+        {
+            return _database.QueryAsync<TodoItem>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+        }
+
+        public Task<TodoItem> GetItemAsync(int id)
+        {
+            return _database.Table<TodoItem>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        }
+        public Task<int> SaveItemAsync(TodoItem item)
+        {
+            if (item.ID != 0)
+            {
+                return _database.UpdateAsync(item);
+            }
+            else
+            {
+                return _database.InsertAsync(item);
+            }
+
+        }
+
+        public Task DeleteItemAsync(int id)
+        {
+            return _database.DeleteAsync(id);
+        }
+
+        internal async Task DeleteItemAsync(TodoItem todoItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Todo/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Models
+{
+    internal class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
